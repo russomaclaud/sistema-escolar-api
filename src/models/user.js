0fe6d185a839c427ce8f278cabde0f9eec518be6
russomaclaud <erussomaclaud@gmail.com>
2022-10-06 21:50:01 -0500

    Modelo usuarios lista

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
             trim: true,
             required: [true, 'Por favor ingresa un código'],
         },
-        numero_documento: {
+        numeroDocumento: {
             type: Number,
             min: 6,
             unique: true,
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema(
             lowercase: true,
             required: [true, 'Por favor ingresa el apellido'],
         },
-        telefono_fijo: {
+        telefonoFijo: {
             type: String,
             minLength: [6, 'El teléfono fijo no debe de ser menor a 6 números'],
             maxLength: [15, 'El teléfono fijo no debe ser mayor a 15 números'],
@@ -60,21 +60,29 @@ const userSchema = new mongoose.Schema(
             min: [19, 'La dirección no debe ser menor de 19 caracteres'],
             maxLength: [100, 'La dirección no debe ser mayor a 100 caracteres'],
         },
-        experiencia_laboral: {
+        experienciaLaboral: {
             type: String,
             trim: true,
         },
-        formacion_academica: {
+        formacionAcademica: {
             type: String,
             trim: true,
         },
-        fecha_exp: {
+        fechaExp: {
             type: Date,
             required: [true, 'Selecciona una fecha'],
         }, // año, mes, día
-        fecha_nacimiento: { type: Date },
-        fecha_inscripcion: { type: Date },
-        fecha_expulsion: { type: Date },
+        fechaNacimiento: { type: Date },
+        fechaInscripcion: { type: Date },
+        estadoAcademicoAnterior: { 
+            type: String,
+            enum: {
+                values: ['aprovado', 'perdido'],
+                message:
+                    'Por favor seleccione la opción correcta si tiene certificado',
+            },
+         },
+        fechaExpulsion: { type: Date },
         certificado: {
             type: String,
             enum: {
@@ -87,7 +95,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             minLength: [6, 'El password no debe ser menor a 6 caracteres'],
         },
-        responsabilidad_economica: {
+        responsabilidadEconomica: {
             type: String,
             enum: ['si', 'no'],
         },
@@ -103,19 +111,19 @@ const userSchema = new mongoose.Schema(
             type: String,
             trim: true,
         },
-        estudiante_Cabeza_familia: {
+        estudianteCabezaFamilia: {
             type: String,
             enum: ['si', 'no'],
         },
-        hijo_madre_cabeza_familia: {
+        hijoMadreCabezaFamilia: {
             type: String,
             enum: ['si', 'no'],
         },
-        hijo_veterano_fuerza_publica: {
+        hijoVeteranoFuerzaPublica: {
             type: String,
             enum: ['si', 'no'],
         },
-        hijo_heroe_nacion: {
+        hijoHeroeNacion: {
             type: String,
             enum: ['si', 'no'],
         },
@@ -123,7 +131,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             default: 'user_default.png',
         },
-        estado_civil: {
+        estadoCivil: {
             type: String,
             enum: {
                 values: [
@@ -140,11 +148,11 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: [true, 'Selecione un genero'],
             enum: {
-                values: ['Femenino', 'Masculino', 'No tiene idea'],
+                values: ['Femenino', 'Masculino', 'No definido'],
                 message: 'Por favor selecciona el genero correcto',
             },
         },
-        grupo_sanguineo: {
+        grupoSanguineo: {
             type: String,
             required: [true, 'Seleccione un grupo sanguineo'],
             enum: {
@@ -156,7 +164,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: [true, 'Seleccione un rol'],
             enum: {
-                values: ['lider', 'docente', 'papitos', 'estudiante', 'admin'],
+                values: ['lider', 'docente', 'papito', 'estudiante', 'admin'],
                 mesage: 'Por favor seleciona el rol correcto',
             },
         },
